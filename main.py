3c8d16f4c1875650dfe101a3ae87155f4e62e347
Jayachandra <100439113+J-Chandra-py@users.noreply.github.com>
2022-10-14 11:29:09 +0000

    with try and except

--- main.py
+++ main.py
@@ -32,11 +32,28 @@ scissors = '''
 hand = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.)"))
-print(hand[user_choice])
+try:
+  print(hand[user_choice])
 
-print("Computer chose:")
-computer_choice = random.randint(0,2)
-print(hand[computer_choice])
+  print("Computer chose:")
+  computer_choice = random.randint(0,2)
+  print(hand[computer_choice])
+
+  if user_choice == 0 and computer_choice == 2:
+    print("You win")
+  elif user_choice == 2 and computer_choice == 0:
+    print("You lose")
+  elif computer_choice > user_choice:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You win")
+  elif computer_choice == user_choice:
+    print("It's a draw")
+  
+except IndexError:
+  #if user_choice >= 3 or user_choice < 0:
+  print("You've typed an invalid number, you lose.")
+
 
 # if user_choice == rock:
 #   if computer_choice == scissors:
@@ -61,14 +78,3 @@ print(hand[computer_choice])
 #   else:
 #     print("Draw")
 
-if user_choice == 0 and computer_choice == 2:
-  print("You win")
-elif user_choice == 2 and computer_choice == 0:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win")
-elif computer_choice == user_choice:
-  print("It's a draw")
-  
